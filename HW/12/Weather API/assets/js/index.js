ceeb1879c9cb5bded39b51bbeddc80df1a3bcdc2
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2022-05-19 20:32:09 +0430

    js for modal and inputs

--- HW/12/Weather API/assets/js/index.js
+++ HW/12/Weather API/assets/js/index.js
@@ -1,1 +1,25 @@
 //API key 1e7177a851febb02355b0bb2d20048db
+const modalBG = document.querySelector(".modal-bg");
+const modal = document.querySelector(".modal");
+const cityInput = document.getElementById("city-name");
+const close = document.getElementById("close");
+const geoInput = document.getElementById("geo-name-input")
+function showModal(){
+    modalBG.style.display = "flex";
+}
+function hideModal(){
+    modalBG.style.display = "none";
+}
+
+cityInput.addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {
+        showModal();
+    }
+});
+geoInput.addEventListener("click", ()=>{showModal()})
+close.addEventListener("click", ()=>{hideModal()})
+
+modalBG.addEventListener("click", ()=>{hideModal()})
+modal.addEventListener("click", (event) => {
+    event.stopPropagation();
+}, false);
