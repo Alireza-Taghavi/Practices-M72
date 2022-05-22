63ec16e89fdb192346f39f9fc35725a7ea7986dd
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2022-05-23 03:59:41 +0430

    finished

--- HW/12/News API/assets/js/index.js
+++ HW/12/News API/assets/js/index.js
@@ -1,13 +1,48 @@
 //API key 77a6e3588ad046b08e4f436d6ab65a83
-var url = 'https://newsapi.org/v2/everything?' +
-    'q=Apple&' +
-    'from=2022-05-22&' +
-    'sortBy=popularity&' +
-    'apiKey=77a6e3588ad046b08e4f436d6ab65a83';
-
-var req = new Request(url);
-
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })
+//DOM
+const input = document.getElementById("search");
+const submit = document.getElementById("submit");
+const container = document.querySelector(".row");
+class Main {
+    constructor(){
+    }
+    fetchNews =  async() =>{
+        const subject = input.value;
+        input.value = "";
+        this.res = await fetch(`https://newsapi.org/v2/everything?q=${subject}&from=2022-05-22&sortBy=popularity&apiKey=77a6e3588ad046b08e4f436d6ab65a83`);
+        this.response = await this.res.json();
+        this.news = this.response.articles;
+        await this.validation();
+    }
+    validation=()=>{
+        if(this.news.length > 0){
+            container.innerHTML = '';
+            this.articleAdder();
+        }
+        else {
+        }
+    }
+    articleAdder=()=>{
+        console.log(this.news)
+        this.news.forEach(this.articleMaker)
+    }
+    articleMaker=(x)=>{
+        container.innerHTML += `<div class="card p-3 col-xl-4 col-4 m-3" style="width: 20rem;">
+            <img alt="${x.title}" src="${x.urlToImage}" class="card-img-top" alt="...">
+            <hr>
+            <div class="card-body">
+                <h5 class="card-title">${x.title}</h5>
+                <a href="${x.url}" class="card-text">${x.description}</a>
+                <hr>
+                <span class="btn btn-primary">${x.publishedAt.substring(0, 10)}</span>
+            </div>
+        </div>`
+    }
+}
+const realMain = new Main();
+submit.addEventListener("click", realMain.fetchNews);
+input.addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {
+        realMain.fetchNews();
+    }
+});
