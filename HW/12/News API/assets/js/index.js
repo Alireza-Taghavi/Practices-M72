fa3a04f5668f9dc40710f330b0868cfb29ed450f
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2022-05-22 21:27:20 +0430

    backup

--- HW/12/News API/assets/js/index.js
+++ HW/12/News API/assets/js/index.js
@@ -1,1 +1,13 @@
 //API key 77a6e3588ad046b08e4f436d6ab65a83
+var url = 'https://newsapi.org/v2/everything?' +
+    'q=Apple&' +
+    'from=2022-05-22&' +
+    'sortBy=popularity&' +
+    'apiKey=77a6e3588ad046b08e4f436d6ab65a83';
+
+var req = new Request(url);
+
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })
