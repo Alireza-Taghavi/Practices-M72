d29095196119785f8bbafe7b0dbc4ee4a9657197
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2022-05-23 17:15:46 +0430

    some stuff

---
+++ HW/13/my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="App-header">
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
