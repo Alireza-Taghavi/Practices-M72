b0ba55977f25a1ba3ad300a0284c73c7453baf11
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2022-05-24 15:55:33 +0430

    functionality

--- HW/13/todo/src/App.js
+++ HW/13/todo/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="todo-app">
-      <h1>To do app</h1>
+      <TodoList />
     </div>
   );
 }
