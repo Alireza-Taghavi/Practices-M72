1d1e4625b6a1683cff3c8e86c5cda53761e91a26
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2022-07-16 01:24:59 +0430

    [HW018] rewrote the front bcs I hate myself more than backend

--- HW/18/front/pages/index.js
+++ HW/18/front/pages/index.js
@@ -12,9 +12,9 @@ export default function Home() {
                 <div className="p-4 flex flex-col gap-4 bg-[#293145] rounded-lg">
                     <div
                         className="border-b border-solid border-[#313A55] flex flex-row justify-between items-center pb-4">
-                        <button className="w-4 h-4 bg-transparent relative top-0 right-0">
+                        <button className="w-6 h-6 flex items-center justify-center bg-transparent relative top-0 right-0 rounded-full bell">
                             <Image alt="bellIcon" src={bellIcon} className="w-4 h-4"/>
-                            <div className="w-[10px] h-[10px] bg-[#E95151] rounded-full flex items-center justify-center text-[8px] font-normal absolute top-[-2px] right-[-2px]">4</div>
+                            <div className="w-[10px] h-[10px] bg-[#E95151] rounded-full flex items-center justify-center text-[8px] font-normal absolute top-[0px] right-[0px]">4</div>
                         </button>
                         <p className="text-xs font-normal">تعداد پشتیبان آنلاین : ۸ نفر</p>
                     </div>
